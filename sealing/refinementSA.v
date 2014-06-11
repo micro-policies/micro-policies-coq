63281f5b7b4bdfeb5b586281cab64e7a2071b1ea
Catalin Hritcu <catalin.hritcu@gmail.com>
2014-06-11 16:19:06 +0200

    [minor] 2 alternatives for instantiating mkkey_f

--- sealing/refinementSA.v
+++ sealing/refinementSA.v
@@ -1,3 +1,5 @@
+Require Import List. Import ListNotations.
+Require Import Coq.Classes.SetoidDec.
 Require Import utils common symbolic.
 Require Import symbolic_sealing sealing.classes sealing.abstract.
 
@@ -60,14 +62,60 @@ Definition refine_pc (w : word t) (a : atom (word t) SymSeal.stag) : Prop :=
   | _               => False
   end.
 
+Variable min_key : key. (* the initial key for the symbolic machine *)
+
+(* Instantiating mkkey_f at abstract level to something
+   corresponding to what's happening at the symbolic level. *)
+
+(* Alternative 1 *)
+Variable key_lt : key -> key -> bool.
+Definition kmax (ks : list key) : key :=
+   List.fold_left (fun kmax k => if key_lt kmax k then k else kmax) ks min_key.
+
+Definition inc_key_opt k :=
+  if k == max_key then None
+                  else Some (inc_key k).
+
+Import DoNotation.
+
+Definition mkkey_f1 (ks : list key) : option (list key * key) :=
+  do k <- inc_key_opt (kmax ks);
+  Some (k::ks,k).
+
+(* Alternative 2: assuming we start with empty list and key_min;
+   doesn't seem to help so much -- ordering still needed in the
+   key_generator instance proof *)
+
+Definition mkkey_f2 (ks : list key) : option (list key * key) :=
+  do k' <- (match ks with
+            | []     => Some min_key
+            | k :: _ => inc_key_opt k
+            end);
+  Some (k'::ks, k').
+
+Variable inc_different : forall k, ~(k = inc_key k).
+
+Program Instance gk_instance2 : AbsSeal.key_generator := {
+  mkkey_f := mkkey_f2
+}.
+Next Obligation.
+  unfold mkkey_f2 in *. destruct ks as [| k' ks].
+  - simpl in *. inversion H; subst. unfold incl. simpl. tauto.
+  - unfold inc_key_opt in *.
+    destruct (equiv_dec k' max_key); simpl in *. discriminate H.
+    inversion H; subst. unfold incl; simpl.
+    split. intros [Hc | Hc]. eapply inc_different; eassumption.
+    admit. (* this seems to rely on keys being _ordered_;
+              we would probably need to make it part of the list key
+              type (refinement) ... not much better than computing max *)
+    split; tauto.
+Admitted.
+
 Definition refine_ins (keys : list key) (next_key : key) : Prop :=
-True.
-(*
-  next_key = key_incr (key_max keys).
-keys = 0,1,2,3
-next_key = 4
-Probably need to talk about the initial key (min_key?)
-*)
+  match mkkey_f2 keys with
+  | Some (_, k) => k = next_key
+  | _           => True (* ??? *)
+  end.
 
 Definition astate := @AbsSeal.state t sk amemory aregisters.
 Definition sstate := @Symbolic.state t SymSeal.sym_sealing.
@@ -79,10 +127,6 @@ Definition refine_state (ast : astate) (sst : sstate) : Prop :=
   refine_reg areg sreg /\
   refine_pc apc spc /\
   refine_ins akeys skey.
-
-(* Should instantiate mkkey_f at abstract level to something
-   corresponding to what's happening at the symbolic level,
-   and provide a key_generator instance (prove mkkey_fresh) *)
 
 (* also refinement for our 3 system calls ... the abstract ones only
    have a description as step rules *)
