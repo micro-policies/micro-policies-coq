aa0f7b60ea8e1cad23ec745ac0c356f48002adb4
Catalin Hritcu <catalin.hritcu@gmail.com>
2014-06-11 09:52:08 +0200

    [minor] back to less than 80 characters lines

--- sealing/abstract.v
+++ sealing/abstract.v
@@ -62,66 +62,78 @@ Definition decode mem pc :=
   end.
 
 Inductive step (st st' : state) : Prop :=
-| step_nop : forall mem reg pc es (ST : st = State mem reg pc es) 
+| step_nop : forall mem reg pc es
+      (ST : st = State mem reg pc es)
     (INST : decode mem pc =? Nop _)
     (NEXT : st' = State mem reg (pc.+1) es),   step st st'
-| step_const : forall mem reg reg' pc n r es (ST : st = State mem reg pc es) 
+| step_const : forall mem reg reg' pc n r es
+      (ST : st = State mem reg pc es)
     (INST : decode mem pc =? Const _ n r)
     (UPD  : upd_reg reg r (VWord (imm_to_word n)) =? reg')
     (NEXT : st' = State mem reg' (pc.+1) es),   step st st'
-| step_mov : forall mem reg reg' pc r1 v1 r2 es (ST : st = State mem reg pc es) 
+| step_mov : forall mem reg reg' pc r1 v1 r2 es
+      (ST : st = State mem reg pc es)
     (INST : decode mem pc =? Mov _ r1 r2)
     (R1W  : get_reg reg r1 =? v1)
     (UPD  : upd_reg reg r2 v1 =? reg')
     (NEXT : st' = State mem reg' (pc.+1) es),   step st st'
-| step_binop : forall mem reg reg' pc op r1 r2 r3 w1 w2 es (ST : st = State mem reg pc es) 
+| step_binop : forall mem reg reg' pc op r1 r2 r3 w1 w2 es
+      (ST : st = State mem reg pc es)
     (INST : decode mem pc =? Binop _ op r1 r2 r3)
     (R1W  : get_reg reg r1 =? VWord w1)
     (R2W  : get_reg reg r2 =? VWord w2)
     (UPD  : upd_reg reg r3 (VWord (binop_denote op w1 w2)) =? reg')
     (NEXT : st' = State mem reg' (pc.+1) es),   step st st'
-| step_load : forall mem reg reg' pc r1 r2 w1 v2 es (ST : st = State mem reg pc es) 
+| step_load : forall mem reg reg' pc r1 r2 w1 v2 es
+      (ST : st = State mem reg pc es)
     (INST : decode mem pc =? Load _ r1 r2)
     (R1W  : get_reg reg r1 =? VWord w1)
     (MEM1 : get_mem mem w1 =? v2)
     (UPD  : upd_reg reg r2 v2 =? reg')
     (NEXT : st' = State mem reg' (pc.+1) es),   step st st'
-| step_store : forall mem mem' reg pc r1 r2 w1 v2 es (ST : st = State mem reg pc es) 
+| step_store : forall mem mem' reg pc r1 r2 w1 v2 es
+      (ST : st = State mem reg pc es)
     (INST : decode mem pc =? Store _ r1 r2)
     (R1W  : get_reg reg r1 =? VWord w1)
     (R2W  : get_reg reg r2 =? v2)
     (UPDM : upd_mem mem w1 v2 =? mem')
     (NEXT : st' = State mem' reg (pc.+1) es),   step st st'
-| step_jump : forall mem reg pc r w es (ST : st = State mem reg pc es) 
+| step_jump : forall mem reg pc r w es
+      (ST : st = State mem reg pc es)
     (INST : decode mem pc =? Jump _ r)
     (RW   : get_reg reg r =? VWord w)
     (NEXT : st' = State mem reg w es),   step st st'
-| step_bnz : forall mem reg pc r n w es (ST : st = State mem reg pc es) 
+| step_bnz : forall mem reg pc r n w es
+      (ST : st = State mem reg pc es)
     (INST : decode mem pc =? Bnz _ r n)
     (RW   : get_reg reg r =? VWord w),
     let pc' := add_word pc (if w ==b Z_to_word 0
                             then Z_to_word 1 else imm_to_word n) in forall
     (NEXT : st' = State mem reg pc' es),   step st st'
-| step_jal : forall mem reg reg' pc r w es (ST : st = State mem reg pc es) 
+| step_jal : forall mem reg reg' pc r w es
+      (ST : st = State mem reg pc es)
     (INST : decode mem pc =? Jal _ r)
     (RW   : get_reg reg r =? VWord w)
     (NOSC : ~In w syscall_addrs)
     (UPD  : upd_reg reg ra (VWord (pc.+1)) =? reg')
     (NEXT : st' = State mem reg' w es),   step st st'
-| step_mkkey : forall mem reg reg' pc r es es' nk (ST : st = State mem reg pc es) 
+| step_mkkey : forall mem reg reg' pc r es es' nk
+      (ST : st = State mem reg pc es)
     (INST : decode mem pc =? Jal _ r)
     (RW   : get_reg reg r =? VWord mkkey_addr)
     (GEN  : mkkey_f es =? (es',nk))
     (UPD  : upd_reg reg syscall_ret (VKey nk) =? reg')
     (NEXT : st' = State mem reg' pc es'),   step st st'
-| step_seal : forall mem reg reg' pc r es payload key (ST : st = State mem reg pc es) 
+| step_seal : forall mem reg reg' pc r es payload key
+      (ST : st = State mem reg pc es)
     (INST : decode mem pc =? Jal _ r)
     (RW   : get_reg reg r =? VWord seal_addr)
     (R1   : get_reg reg syscall_arg1 =? VWord payload)
     (R2   : get_reg reg syscall_arg2 =? VKey key)
     (UPD  : upd_reg reg syscall_ret (VSealed payload key) =? reg')
     (NEXT : st' = State mem reg' pc es),   step st st'
-| step_unseal : forall mem reg reg' pc r es payload key (ST : st = State mem reg pc es) 
+| step_unseal : forall mem reg reg' pc r es payload key
+      (ST : st = State mem reg pc es)
     (INST : decode mem pc =? Jal _ r)
     (RW   : get_reg reg r =? VWord unseal_addr)
     (R1   : get_reg reg syscall_arg1 =? VSealed payload key)
