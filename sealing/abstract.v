ad4c227dfd4f14da2918e5e9bde0e53fa1699f3e
Benjamin Pierce <bcpierce@cis.upenn.edu>
2014-06-11 15:53:39 -0400

    [minor] working on the tex

--- sealing/abstract.v
+++ sealing/abstract.v
@@ -64,73 +64,71 @@ Definition decode (mem : memory) (pc : word t) :=
     | _              => None
   end.
 
-(* BCP: OK, I like the way this looks now... *)
-
 Inductive step (st st' : state) : Prop :=
 | step_nop : forall mem reg pc es
-      (ST : st = State mem reg pc es)
+    (ST   : st = State mem reg pc es)
     (INST : decode mem pc =? Nop _)
     (NEXT : st' = State mem reg (pc.+1) es),   step st st'
 | step_const : forall mem reg reg' pc n r es
-      (ST : st = State mem reg pc es)
+    (ST   : st = State mem reg pc es)
     (INST : decode mem pc =? Const _ n r)
     (UPD  : upd reg r (VData (imm_to_word n)) =? reg')
     (NEXT : st' = State mem reg' (pc.+1) es),   step st st'
 | step_mov : forall mem reg reg' pc r1 v1 r2 es
-      (ST : st = State mem reg pc es)
+    (ST   : st = State mem reg pc es)
     (INST : decode mem pc =? Mov _ r1 r2)
     (R1W  : get reg r1 =? v1)
     (UPD  : upd reg r2 v1 =? reg')
     (NEXT : st' = State mem reg' (pc.+1) es),   step st st'
 | step_binop : forall mem reg reg' pc op r1 r2 r3 w1 w2 es
-      (ST : st = State mem reg pc es)
+    (ST   : st = State mem reg pc es)
     (INST : decode mem pc =? Binop _ op r1 r2 r3)
     (R1W  : get reg r1 =? VData w1)
     (R2W  : get reg r2 =? VData w2)
     (UPD  : upd reg r3 (VData (binop_denote op w1 w2)) =? reg')
     (NEXT : st' = State mem reg' (pc.+1) es),   step st st'
 | step_load : forall mem reg reg' pc r1 r2 w1 v2 es
-      (ST : st = State mem reg pc es)
+    (ST   : st = State mem reg pc es)
     (INST : decode mem pc =? Load _ r1 r2)
     (R1W  : get reg r1 =? VData w1)
     (MEM1 : get mem w1 =? v2)
     (UPD  : upd reg r2 v2 =? reg')
     (NEXT : st' = State mem reg' (pc.+1) es),   step st st'
 | step_store : forall mem mem' reg pc r1 r2 w1 v2 es
-      (ST : st = State mem reg pc es)
+    (ST   : st = State mem reg pc es)
     (INST : decode mem pc =? Store _ r1 r2)
     (R1W  : get reg r1 =? VData w1)
     (R2W  : get reg r2 =? v2)
     (UPDM : upd mem w1 v2 =? mem')
     (NEXT : st' = State mem' reg (pc.+1) es),   step st st'
 | step_jump : forall mem reg pc r w es
-      (ST : st = State mem reg pc es)
+    (ST   : st = State mem reg pc es)
     (INST : decode mem pc =? Jump _ r)
     (RW   : get reg r =? VData w)
     (NEXT : st' = State mem reg w es),   step st st'
 | step_bnz : forall mem reg pc r n w es
-      (ST : st = State mem reg pc es)
+    (ST   : st = State mem reg pc es)
     (INST : decode mem pc =? Bnz _ r n)
     (RW   : get reg r =? VData w),
     let pc' := add_word pc (if w ==b Z_to_word 0
                             then Z_to_word 1 else imm_to_word n) in forall
     (NEXT : st' = State mem reg pc' es),   step st st'
 | step_jal : forall mem reg reg' pc r w es
-      (ST : st = State mem reg pc es)
+    (ST   : st = State mem reg pc es)
     (INST : decode mem pc =? Jal _ r)
     (RW   : get reg r =? VData w)
     (NOSC : ~In w syscall_addrs)
     (UPD  : upd reg ra (VData (pc.+1)) =? reg')
     (NEXT : st' = State mem reg' w es),   step st st'
 | step_mkkey : forall mem reg reg' pc r es es' nk
-      (ST : st = State mem reg pc es)
+    (ST   : st = State mem reg pc es)
     (INST : decode mem pc =? Jal _ r)
     (RW   : get reg r =? VData mkkey_addr)
     (GEN  : mkkey_f es =? (es',nk))
     (UPD  : upd reg syscall_ret (VKey nk) =? reg')
     (NEXT : st' = State mem reg' pc es'),   step st st'
 | step_seal : forall mem reg reg' pc r es payload key
-      (ST : st = State mem reg pc es)
+    (ST   : st = State mem reg pc es)
     (INST : decode mem pc =? Jal _ r)
     (RW   : get reg r =? VData seal_addr)
     (R1   : get reg syscall_arg1 =? VData payload)
@@ -138,7 +136,7 @@ Inductive step (st st' : state) : Prop :=
     (UPD  : upd reg syscall_ret (VSealed payload key) =? reg')
     (NEXT : st' = State mem reg' pc es),   step st st'
 | step_unseal : forall mem reg reg' pc r es payload key
-      (ST : st = State mem reg pc es)
+    (ST   : st = State mem reg pc es)
     (INST : decode mem pc =? Jal _ r)
     (RW   : get reg r =? VData unseal_addr)
     (R1   : get reg syscall_arg1 =? VSealed payload key)
