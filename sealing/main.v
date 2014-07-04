72c3d2c011896e86ceddd297eacf4e8748bf90d6
Arthur Azevedo de Amorim <aarthur@seas.upenn.edu>
2014-07-04 21:13:38 +0200

    [minor] Fix broken definition.

--- sealing/main.v
+++ sealing/main.v
@@ -641,19 +641,17 @@ Definition run_abs n p :=
    map (summarize_abstract_state 8) tr
   ).
 
-(* ARGH -- broken by Arthur's parallel change -- help!
 
 Definition run_sym n p :=
   let: (init,syscall_addrs) := build_symbolic_sealing_machine p in
   let tr := utils.runn (fun x => @Symbolic.stepf concrete_int_32_t concrete_int_32_ops
-                                 (@Sym.sym_sealing t sk_defs sp)
+                                 (@Sym.sym_sealing sk_defs)
                                  Sym.sealing_syscalls
                                  x) n init in
   (
    summarize_symbolic_sealing_state 3000 init ::
    map (summarize_symbolic_sealing_state 8) tr
   ).
-*)
 
 Definition hello_world0 : @relocatable_segment t (list w) (instr concrete_int_32_t) :=
   user_code (fun _ _ => [
