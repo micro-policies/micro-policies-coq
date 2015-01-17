f171222d28e59af9f153622bbd8837e8eadac382
Arthur Azevedo de Amorim <aarthur@seas.upenn.edu>
2015-01-17 09:20:46 -0500

    Move most of ranges to utils.

--- compartmentalization/ranges.v
+++ compartmentalization/ranges.v
@@ -1,8 +1,6 @@
-Require Import ssreflect ssrfun ssrbool eqtype ssrnat seq fintype.
-Require Import div ssrint intdiv.
-Require Import ord word partmap.
-Require Import lib.utils common.common.
-Set Bullet Behavior "Strict Subproofs".
+Require Import ssreflect ssrfun ssrbool eqtype ssrnat.
+Require Import ord word.
+Require Import common.common.
 
 Section WithClasses.
 
@@ -14,44 +12,6 @@ Local Notation word := (mword t).
 Open Scope word_scope.
 Open Scope ord_scope.
 
-(*
-Lemma addw_succ : forall w1 w2 : word,
-  w1 < w2 -> Word.unsigned (w1 + 1)%w = (Word.unsigned w1 + 1)%Z.
-Proof.
-  move => x y LT.
-  rewrite -{1}(Word.repr_unsigned _ x) /Word.one Word.add_repr.
-  apply Word.unsigned_repr.
-  rewrite /lt IntOrdered.compare_unsigned in LT.
-  move: (Word.unsigned_range x) (Word.unsigned_range y) => H1 H2.
-  have H3: (Word.unsigned x < Word.unsigned y)%Z by auto.
-  rewrite /Word.max_unsigned.
-  omega.
-Qed.
-*)
-
-Lemma leqw_succ : forall x y : word, x < y -> x < x + 1.
-Proof.
-move=> [[x Px]] [[y Py]]; do !rewrite !/Ord.leq /=.
-rewrite -!ltnNge !modz_nat !absz_nat !modn_mod.
-case: (word_size t) Px Py => [|k Px Py Pxy] /=.
-  by rewrite expn0 modn1; case: x y => [|x] [|y].
-rewrite !modn_small ?(addn1, leqnn) //;
-  try by rewrite -{1}(expn0 2) ltn_exp2l.
-by apply: (@leq_trans y.+1).
-Qed.
-
 Definition range (l h : word) := [pred e | l <= e <= h].
-
-Lemma addw_le : forall x y : word,
-  x < y -> x + 1 <= y.
-Proof.
-move=> [[x Px]] [[y Py]]; do !rewrite !/Ord.leq /=.
-rewrite -!ltnNge /= !modz_nat !absz_nat !modn_mod.
-case: (word_size t) Px Py => [|k Px Py Pxy] /=.
-  by rewrite expn0 modn1; case: x y => [|x] [|y].
-rewrite !modn_small ?(addn1, leqnn) //;
-  try by rewrite -{1}(expn0 2) ltn_exp2l.
-by apply: (@leq_trans y.+1).
-Qed.
 
 End WithClasses.
---
+++ lib/word_utils.v
@@ -0,0 +1,47 @@
+Require Import ssreflect ssrbool ssrfun eqtype ssrnat div ssrint intdiv.
+Require Import ord word.
+
+Section WordUtils.
+
+Local Open Scope word_scope.
+Local Open Scope ord_scope.
+
+(*
+Lemma addw_succ : forall w1 w2 : word,
+  w1 < w2 -> Word.unsigned (w1 + 1)%w = (Word.unsigned w1 + 1)%Z.
+Proof.
+  move => x y LT.
+  rewrite -{1}(Word.repr_unsigned _ x) /Word.one Word.add_repr.
+  apply Word.unsigned_repr.
+  rewrite /lt IntOrdered.compare_unsigned in LT.
+  move: (Word.unsigned_range x) (Word.unsigned_range y) => H1 H2.
+  have H3: (Word.unsigned x < Word.unsigned y)%Z by auto.
+  rewrite /Word.max_unsigned.
+  omega.
+Qed.
+*)
+
+Lemma leqw_succ : forall n (x y : word n), x < y -> x < x + 1.
+Proof.
+move=> n [[x Px]] [[y Py]]; do !rewrite !/Ord.leq /=.
+rewrite -!ltnNge !modz_nat !absz_nat !modn_mod.
+case: n Px Py => [|k Px Py Pxy] /=.
+  by rewrite expn0 modn1; case: x y => [|x] [|y].
+rewrite !modn_small ?(addn1, leqnn) //;
+  try by rewrite -{1}(expn0 2) ltn_exp2l.
+by apply: (@leq_trans y.+1).
+Qed.
+
+Lemma addw_le : forall n (x y : word n),
+  x < y -> x + 1 <= y.
+Proof.
+move=> n [[x Px]] [[y Py]]; do !rewrite !/Ord.leq /=.
+rewrite -!ltnNge /= !modz_nat !absz_nat !modn_mod.
+case: n Px Py => [|k Px Py Pxy] /=.
+  by rewrite expn0 modn1; case: x y => [|x] [|y].
+rewrite !modn_small ?(addn1, leqnn) //;
+  try by rewrite -{1}(expn0 2) ltn_exp2l.
+by apply: (@leq_trans y.+1).
+Qed.
+
+End WordUtils.
