e5a9825ab9ab61b0277ec9b1b42112e68effea6b
Arthur Azevedo de Amorim <aarthur@seas.upenn.edu>
2015-02-06 00:15:37 -0500

    Adapt remaining policies.

--- compartmentalization/symbolic.v
+++ compartmentalization/symbolic.v
@@ -447,11 +447,11 @@ Definition add_to_store_targets (s : Symbolic.state mt)
       Some (Symbolic.State M_next R_next (pc' @ (PC JUMPED c_sys)) si_next)
   end.
 
-Definition syscalls : seq (Symbolic.syscall mt) :=
+Definition syscalls : Symbolic.syscall_table mt :=
   let dummy := DATA 0%w set0 set0 in
-  [:: Symbolic.Syscall isolate_addr              dummy isolate;
-      Symbolic.Syscall add_to_jump_targets_addr  dummy add_to_jump_targets;
-      Symbolic.Syscall add_to_store_targets_addr dummy add_to_store_targets].
+  [partmap (isolate_addr,              Symbolic.Syscall dummy isolate);
+           (add_to_jump_targets_addr,  Symbolic.Syscall dummy add_to_jump_targets);
+           (add_to_store_targets_addr, Symbolic.Syscall dummy add_to_store_targets)].
 
 Definition step := Symbolic.step syscalls.
 
