9061370d7697feee193672a57286e79f316022b9
Arthur Azevedo de Amorim <aarthur@seas.upenn.edu>
2015-02-13 16:56:33 -0500

    Cover more cases in property definition.

--- memory_safety/propertyA.v
+++ memory_safety/propertyA.v
@@ -15,65 +15,160 @@ Unset Printing Implicit Defensive.
 
 Section MemorySafety.
 
+Local Open Scope fset_scope.
+
+Import Abstract.
+
 Variable mt : machine_types.
 Variable ops : machine_ops mt.
 Variable sr : syscall_regs mt.
 Variable block : ordType.
-Variable allocator : Abstract.allocator mt block.
+Variable alloc : allocator mt block.
 Variable addrs : memory_syscall_addrs mt.
 
-Hypothesis allocatorP : Abstract.allocator_spec allocator.
+Hypothesis allocP : allocator_spec alloc.
 
-Local Notation state := (Abstract.state mt block).
-Local Notation pointer := [eqType of Abstract.pointer mt block].
+Local Notation state := (state mt block).
+Local Notation pointer := [eqType of pointer mt block].
+Local Notation value := (value mt block).
 
+Implicit Type m : memory mt block.
+Implicit Type rs : registers mt block.
 Implicit Type s : state.
 Implicit Type b : block.
 Implicit Type p : pointer.
 Implicit Type bs : {fset block}.
 
-Definition references s b b' :=
+Definition references m b b' :=
   [exists offs : mword mt * mword mt,
-    Abstract.getv (Abstract.mem s) (b, offs.1) ==
-    Some (Abstract.VPtr (b', offs.2))].
+    getv m (b, offs.1) == Some (VPtr (b', offs.2))].
 
-Inductive reachable s b : Prop :=
-| ReachBase r off of Abstract.regs s r = Some (Abstract.VPtr (b, off))
-| ReachHop b' of reachable s b' & references s b' b.
+Inductive reachable pc rs m b : Prop :=
+| ReachBasePc p of pc = VPtr p & p.1 = b
+| ReachBaseReg r p of rs r = Some (VPtr p) & p.1 = b
+| ReachHop b' of reachable pc rs m b' & references m b' b.
+Hint Constructors reachable.
+
+Definition reachable_blocks pc rs m bs :=
+  forall b, b \in bs <-> reachable pc rs m b.
 
 Definition live_blocks s bs :=
-  (forall b, b \in bs <-> reachable s b) /\
-  (forall b, b \in bs -> b \in Abstract.blocks s).
+  reachable_blocks (pc s) (regs s) (mem s) bs /\
+  (forall b, b \in bs -> b \in blocks s).
 
-CoInductive valid_write s s' bs : Prop :=
-| ValidWrite p v
-  of Abstract.updv (Abstract.mem s) p v = Some (Abstract.mem s')
-  &  p.1 \in bs.
+(* FIXME: Right now, this doesn't say anything about memory reads. *)
+CoInductive valid_step s bs s' bs' : Prop :=
+| ValidNop of mem s = mem s' & {subset bs' <= bs}
+| ValidWrite p v of updv (mem s) p v = Some (mem s')
+                  & {subset bs' <= bs} & p.1 \in bs
+(* FIXME: removing the typing annotation below causes Coq to miss
+   the coercion fset >-> predType. Should probably change the
+   coercion definition to get it to work. *)
+| ValidAlloc b of malloc_fun (mem s) (blocks s) 0%w = (mem s', b)
+                & {subset bs' <= b |: bs : {fset block}}
+| ValidFree b of Abstract.free_fun (Abstract.mem s) b = Some (Abstract.mem s')
+               & {subset bs' <= bs} & b \in bs.
 
-CoInductive valid_free s s' bs : Prop :=
-| ValidFree b
-  of Abstract.free_fun (Abstract.mem s) b = Some (Abstract.mem s')
-  &  b \in bs.
+CoInductive value_ok pc rs m : value -> Prop :=
+| VOkData x : value_ok pc rs m (VData _ x)
+| VOkPtr p of reachable pc rs m p.1 : value_ok pc rs m (VPtr p).
+Hint Constructors value_ok.
 
-Ltac contradict_equal_mem :=
-  match goal with
-  | H : is_true (?x != ?x) |- _ => by rewrite eqxx in H
-  end.
+CoInductive valid_pc_upd (pc pc' : value) rs m : Prop :=
+| ValidPcUpd of value_ok pc rs m pc'.
+Hint Constructors valid_pc_upd.
 
-Lemma change_no_alloc s s' bs :
-  Abstract.step s s' ->
-  live_blocks s bs ->
-  live_blocks s' bs ->
-  Abstract.mem s != Abstract.mem s' ->
-  valid_write s s' bs \/ valid_free s s' bs.
+CoInductive valid_reg_upd pc rs rs' m : Prop :=
+| ValidRegSame of rs = rs'
+| ValidRegUpd v r of updm rs r v = Some rs' & value_ok pc rs m v.
+Hint Constructors valid_reg_upd.
+
+Lemma upd_reachable pc pc' rs rs' m bs bs' :
+  reachable_blocks pc rs m bs ->
+  reachable_blocks pc' rs' m bs' ->
+  valid_pc_upd pc pc' rs m ->
+  valid_reg_upd pc rs rs' m ->
+  {subset bs' <= bs}.
 Proof.
-case: s s' / => /=; try (move=> *; contradict_equal_mem).
-- move=> m m' rs b pc ptr i r1 r2 v _ _ get_ptr _ upd_m [hbs _] _ _.
-  left; econstructor; eauto.
-  apply/hbs; apply/(@ReachBase _ ptr.1 r1 ptr.2); simpl; eauto.
-  by case: {upd_m get_ptr} ptr get_ptr.
-- move=> m m' rs rs' bs' sz b pc' _ hm' hrs' _ [/= /(_ b) hbs /(_ b) hsub]
-         [/= /(_ b) hbs' /(_ b) hsub'] _.
+move=> hbs hbs' v_pc v_rs b /hbs' reach_b; apply/hbs.
+elim: b / reach_b {hbs hbs'} => [b [b' off] /= hpc' hb'|b r [b' off]/=|b b' _].
+- rewrite {}hb' {b'} in hpc'.
+  case: v_pc => v_pc.
+  by case: pc' / v_pc hpc' => [//|[b' off'] /= ? [<- _]].
+- move=> hr hb'; move: hr; rewrite {}hb' {b'}.
+  case: v_rs => [-> hr|v r' upd_rs]; first by eapply ReachBaseReg; eauto.
+  move: {upd_rs} (updm_set upd_rs)=> upd_rs v_ok.
+  rewrite {}upd_rs {rs'} getm_set.
+  have [_{r}[vE]|_ hr] := altP eqP.
+    by case: v / v_ok vE => // - [b' off'] /= ? [<- _].
+  by eapply ReachBaseReg; eauto.
+by eapply ReachHop.
+Qed.
+
+Lemma get_reg_ok pc rs r m v bs :
+  rs r = Some v -> value_ok pc rs m v.
+Proof.
+case: v => [?|[b off get_rs]]; constructor.
+by eapply ReachBaseReg; eauto.
+Qed.
+
+Lemma get_mem_ok pc rs m p v :
+  value_ok pc rs m (VPtr p) ->
+  getv m p = Some v ->
+  value_ok pc rs m v.
+Proof.
+move=> p_ok; move: {1 2}(VPtr p) p_ok (erefl (VPtr p))=> v'.
+case: v' / => // - [b off] b_reach [<-].
+case: v => [?|[b' off' get_p]]; constructor.
+eapply ReachHop; eauto; apply/existsP; exists (off,off')=> /=.
+by apply/eqP.
+Qed.
+
+Lemma lift_binop_ok pc rs m o v1 v2 v3 :
+  value_ok pc rs m v1 ->
+  value_ok pc rs m v2 ->
+  lift_binop o v1 v2 = Some v3 ->
+  value_ok pc rs m v3.
+Proof.
+rewrite /lift_binop.
+case: v1 / => [v1|[b1 off1] hb1]; case: v2 / => [v2|[b2 off2] hb2];
+case: o=> //;
+try match goal with
+| |- context[?b1 == ?b2] =>
+  have [b1_eq_b2|b1_neq_b2] // := altP (b1 =P b2)
+end;
+move=> [<-]; constructor; done.
+Qed.
+
+Ltac simple_intros :=
+  move=> /= *;
+  repeat match goal with
+  | H : live_blocks ?s ?bs |- _ =>
+    let live := fresh "live" in
+    let sub := fresh "sub" in
+    case: H => [/= live sub]
+  end;
+  apply: ValidNop; first done.
+
+Ltac solve_simple_cases :=
+  simple_intros;
+  first [ solve [ eapply upd_reachable; try eassumption;
+                  eauto using get_reg_ok, get_mem_ok, lift_binop_ok;
+                  done ]
+        | failwith "solve_simple_cases" ].
+
+Lemma safe_step s bs s' bs' :
+  step s s' ->
+  live_blocks s bs ->
+  live_blocks s' bs' ->
+  valid_step s bs s' bs'.
+Proof.
+case: s s' / => /=; try solve_simple_cases.
+- move=> m m' rs b pc ptr i r1 r2 v _ _ get_ptr _ upd_m [/= hbs _] [/= hbs' _].
+  eapply ValidWrite; eauto.
+    admit.
+  by apply/hbs; apply/(@ReachBaseReg _ _ _ ptr.1 r1 ptr); simpl; eauto.
+- move=> m m' rs rs' bs'' sz b pc' _ hm' hrs' _ [/= hbs hsub] [/= hbs' hsub'].
   generalize (Abstract.malloc_fresh hm'); rewrite hsub //; apply/hbs'.
   apply: (@ReachBase _ _ syscall_ret 0%w) => /=.
   by rewrite (updm_set hrs') getm_set eqxx.
