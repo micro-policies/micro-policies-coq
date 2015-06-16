38c7c6cac8bf11c02fe10dddc3866bac15bf6a35
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2015-06-16 19:11:47 -0400

    Added some more documentation.

--- os/haskell/Haskell/Assembler.hs
+++ os/haskell/Haskell/Assembler.hs
@@ -1,14 +1,4 @@
 {-# LANGUAGE TupleSections  #-}
-module Haskell.Assembler (
-  module Haskell.Monad.Assembler,
-  SymAssembler,
-  instr, instrs,
-  nop, const_, mov, binop, load, store, jump, bnz, jal,
-  jumpEpc, addRule, getTag, putTag,
-  tryMWordImm,
-  immediateTooBigMsg, addrToImm, addrToImm',
-  hereImm, reserveImm
-  ) where
 
 {-|
 Module      : Haskell.Assembler
@@ -25,6 +15,17 @@ writing instructions, working with immediates vs. machine words, etc.  The
 'SymAssembler' type provides the appropriate type parameters to 'Assembler'.
 -}
 
+module Haskell.Assembler (
+  module Haskell.Monad.Assembler,
+  SymAssembler,
+  instr, instrs,
+  nop, const_, mov, binop, load, store, jump, bnz, jal,
+  jumpEpc, addRule, getTag, putTag,
+  tryMWordImm,
+  immediateTooBigMsg, addrToImm, addrToImm',
+  hereImm, reserveImm
+  ) where
+
 import Control.Arrow
 import Control.Applicative hiding (Const(..))
 import Control.Monad
@@ -34,27 +35,31 @@ import Haskell.Machine
 
 import Haskell.Monad.Assembler
 
+-- |An 'Assembler' monad for the symbolic machine.  Errors are 'String's;
+-- pointers and words are both 'MWord's.
 type SymAssembler = Assembler String MWord MWord
 
+-- |Encodes and writes a single instruction to the instruction stream.
 instr :: Instr -> SymAssembler ()
 instr = asmWord . encodeInstr
 
+-- |Encodes and writes a list of instructions to the instruction stream.
 instrs :: [Instr] -> SymAssembler ()
 instrs = asmWords . map encodeInstr
 
-nop     :: SymAssembler ()
-const_  :: Imm -> Reg -> SymAssembler ()
-mov     :: Reg -> Reg -> SymAssembler ()
-binop   :: Binop -> Reg -> Reg -> Reg -> SymAssembler ()
-load    :: Reg -> Reg -> SymAssembler ()
-store   :: Reg -> Reg -> SymAssembler ()
-jump    :: Reg -> SymAssembler ()
-bnz     :: Reg -> Imm -> SymAssembler ()
-jal     :: Reg -> SymAssembler ()
-jumpEpc :: SymAssembler ()
-addRule :: SymAssembler ()
-getTag  :: Reg -> Reg -> SymAssembler ()
-putTag  :: Reg -> Reg -> Reg -> SymAssembler ()
+nop     :: SymAssembler ()                               -- ^Write a 'Nop' instruction to the instruction stream
+const_  :: Imm -> Reg -> SymAssembler ()                 -- ^Write a 'Const' instruction to the instruction stream
+mov     :: Reg -> Reg -> SymAssembler ()                 -- ^Write a 'Mov' instruction to the instruction stream
+binop   :: Binop -> Reg -> Reg -> Reg -> SymAssembler () -- ^Write a 'Binop' instruction to the instruction stream
+load    :: Reg -> Reg -> SymAssembler ()                 -- ^Write a 'Load' instruction to the instruction stream
+store   :: Reg -> Reg -> SymAssembler ()                 -- ^Write a 'Store' instruction to the instruction stream
+jump    :: Reg -> SymAssembler ()                        -- ^Write a 'Jump' instruction to the instruction stream
+bnz     :: Reg -> Imm -> SymAssembler ()                 -- ^Write a 'Bnz' instruction to the instruction stream
+jal     :: Reg -> SymAssembler ()                        -- ^Write a 'Jal' instruction to the instruction stream
+jumpEpc :: SymAssembler ()                               -- ^Write a 'JumpEpc' instruction to the instruction stream
+addRule :: SymAssembler ()                               -- ^Write an 'AddRule' instruction to the instruction stream
+getTag  :: Reg -> Reg -> SymAssembler ()                 -- ^Write a 'GetTag' instruction to the instruction stream
+putTag  :: Reg -> Reg -> Reg -> SymAssembler ()          -- ^Write a 'PutTag' instruction to the instruction stream
 
 nop     = instr                Nop
 const_  = (instr .)          . Const
@@ -71,6 +76,9 @@ getTag  = (instr .)          . GetTag
 putTag  = ((instr .) .)      . PutTag
 halt    = instr                Halt
 
+-- |@tryMWordImm fImm fWord w@ checks if the word @w@ fits into an immediate.
+-- If it does, then 'fImm' is called on the resulting immediate; if it does not,
+-- 'fWord' is called on the original word.
 tryMWordImm :: (Imm -> a) -> (MWord -> a) -> MWord -> a
 tryMWordImm fImm fWord w =
   let n = unsignedWord $ mwordWord w
@@ -78,20 +86,34 @@ tryMWordImm fImm fWord w =
      then fImm $ imm n
      else fWord w
                                                                          
+-- |An error message to display when the given word is too big to fit into an
+-- immediate.
 immediateTooBigMsg :: MWord -> String
 immediateTooBigMsg a = "Address " ++ show a ++ " is too big to be immediate."
 
+-- |Convert a word into an immediate, failing /immediately/ if the word was out
+-- of range.  Do not use with time-traveling information (such as the result of
+-- 'reserve').
 addrToImm :: MWord -> SymAssembler Imm
 addrToImm = tryMWordImm pure (asmError . immediateTooBigMsg)
 
+-- |Convert a word into an immediate, with a /delayed/ failure if the word was
+-- out of range.  This function is safe to use with time-traveling information
+-- (such as the result of 'reserve').
 addrToImm' :: MWord -> SymAssembler Imm
 addrToImm' = uncurry (<$) . second asmDelayedError
            . tryMWordImm (,Nothing)
                          (   imm . unsignedWord . mwordWord
                          &&& Just . immediateTooBigMsg)
 
+-- |Return the current instruction address as an immediate (see also 'here');
+-- fails immediately if the current instruction address does not fit into an
+-- immediate.
 hereImm :: SymAssembler Imm
 hereImm = addrToImm =<< here
 
+-- |Reserve some data and return its address as an immediate (see also
+-- 'reserve'); causes a delayed failure if the current instruction address does
+-- not fit into an immediate.
 reserveImm :: MWord -> SymAssembler Imm
 reserveImm = addrToImm' <=< reserve
